a22bb073b909502baee2da05ea89d8b7c2a3b8d8
Philimuhire <philimuhire@gmail.com>
2023-07-14 09:42:59 +0200

    Created changeColorOnHover, getRandomColor, resetGrid functions

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ function createGrid(size) {
   // Generate the initial grid
   createGrid(gridSize);
   
+  // Function to change the color of a grid square on hover
+function changeColorOnHover(square) {
+  square.addEventListener('mouseenter', function () {
+    square.style.backgroundColor = getRandomColor();
+  });
+}
+
+// Helper function to generate a random color
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Add hover effect to each grid square
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+  changeColorOnHover(square);
+});
+
+// Function to prompt the user for grid size and generate a new grid
+function resetGrid() {
+  let newSize = prompt('Enter the number of squares per side (max: 100):');
+  newSize = parseInt(newSize);
+
+  // Validate user input
+  if (isNaN(newSize) || newSize <= 0 || newSize > 100) {
+    alert('Invalid input! Please enter a number between 1 and 100.');
+    return;
+  }
+
+  // Remove the existing grid
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+
+  // Generate the new grid
+  createGrid(newSize);
+}
+
+// Add click event listener to the reset button
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', resetGrid);
