2c374c6cca9223861f6c0e488608faf8624d6cb7
Philimuhire <philimuhire@gmail.com>
2023-07-16 19:38:25 +0200

    modified index.html and script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Function to create the grid
 function createGrid(size) {
-    const container = document.getElementById('container');
-    container.innerHTML = ''; // Clear the container before generating the grid
-  
-    // Calculate the width and height of each square
-    const squareSize = 960 / size;
-  
-    // Loop to generate the grid
-    for (let i = 0; i < size * size; i++) {
-      const square = document.createElement('div');
-      square.style.width = squareSize + 'px';
-      square.style.height = squareSize + 'px';
-      square.classList.add('square'); // Add the .square class
-      container.appendChild(square);
-    }
+  const container = document.getElementById('container');
+  container.innerHTML = ''; // Clear the container before generating the grid
+
+  // Calculate the width and height of each square
+  const squareSize = 960 / size;
+
+  // Loop to generate the grid
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.style.width = squareSize + 'px';
+    square.style.height = squareSize + 'px';
+    square.classList.add('square'); // Add the .square class
+    container.appendChild(square);
+
+    // Add hover effect to each grid square
+    changeColorOnHover(square);
   }
+}
   
   // Initial grid size
   let gridSize = 16;
